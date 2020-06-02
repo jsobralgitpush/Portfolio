57f5ee258bb9267fca29512a551b897bf64e4eed
jsobralgitpush <jose.sobral@poli.ufrj.br>
2020-06-02 18:30:19 -0300

    Desafio 4 Codenation

--- Codenation/Modulo 4 - Backend Skills/RESOLUCAO/main.py
+++ Codenation/Modulo 4 - Backend Skills/RESOLUCAO/main.py
@@ -1,9 +1,19 @@
 import jwt
 
-
 def create_token(data, secret):
-    raise NotImplementedError()
+    token = jwt.encode(data, secret, algorithm = 'HS256')
 
+    return token
 
 def verify_signature(token):
-    raise NotImplementedError()
+    secret = 'acelera'
+    data = {'language': 'Python'}
+
+    real_token = create_token(data, secret)
+
+    if real_token == token:
+        decoded = jwt.decode(token, secret, algorithms='HS256')
+        return decoded
+    else:
+        return {'error':2}
+
