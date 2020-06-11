45e560ba08c91d0e8b9469024e7822780deeda7b
unknown <jose.sobral@poli.ufrj.br>
2020-06-11 18:17:26 -0300

    Testando React

---
+++ Bootcamp Dev Full Stack (IGTI)/3-FrontEnd com React/Exercicios Guiados/JavaScript puro vs JavaScript com React/2-JavaScript com React/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+function pressBotao() {
+  return (
+    <button class='botao'>Date de Hoje</button>
+  );
+}
+
+
+function App() {
+  return (
+    <p>Teste</p>
+  );
+}
+
+export default App;
+exports pressBotao
---
+++ Bootcamp Dev Full Stack (IGTI)/3-FrontEnd com React/Exercicios Guiados/JavaScript puro vs JavaScript com React/2-JavaScript com React/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import pressBotao from './App';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+    <pressBotao />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
